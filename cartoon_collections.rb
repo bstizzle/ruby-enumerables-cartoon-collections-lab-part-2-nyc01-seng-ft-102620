dfdbbdb1dc8aea06c6815b7142eee7848745df32
Benjamin Stein <whereyouben@gmail.com>
2020-10-20 22:48:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element| 
-    element == "Earth!" || elemet == "Wind!"
+    element == "Earth!" || element == "Wind!" element == "Fire!" || element == "Water!" || element == "Heart!"
   end 
   
 end
